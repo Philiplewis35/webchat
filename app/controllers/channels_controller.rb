36dc61fb90ad966ef3740cc30a3d8b8f45566b33
philip <philiplewis35@gmail.com>
2021-05-13 16:29:58 +0100

    channel crud

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,22 +1,52 @@
 class ChannelsController < ApplicationController
+  before_action :set_channel, only: [:show, :edit, :update, :destroy]
+
   def index
+    @channels = Channel.all
+    @admin = current_user.admin?
   end
 
   def new
+    @channel = Channel.new
   end
 
   def create
+    if Channel.create!(channel_params)
+      flash[:success]= t('create.success', record: "channel")
+    else
+      flash[:error]= t('create.error', record: "channel")
+    end
+    redirect_to root_path
   end
 
-  def show
+  def update
+    if @channel.update!(channel_params)
+      flash[:success]= t('update.success', record: "channel")
+    else
+      flash[:error]= t('update.error', record: "channel")
+    end
+    redirect_to channel_path(@channel)
+  end
+
+  def destroy
+    if @channel.destroy!
+      flash[:success]= t('destroy.success', record: "channel")
+    else
+      flash[:error]= t('destroy.error', record: "channel")
+    end
+    redirect_to root_path
   end
 
   def edit
   end
 
-  def update
+  private
+
+  def set_channel
+    @channel = Channel.find(params[:id])
   end
 
-  def destroy
+  def channel_params
+    params.require(:channel).permit!
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
 
-  resources :channel
+  resources :channels
   root to: "channels#index"
 end
