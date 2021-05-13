a139b3eacf7d4f66d635bbb1aa57c6891fa4b5dc
philip <philiplewis35@gmail.com>
2021-05-13 16:16:40 +0100

    channel model

---
+++ db/migrate/20210513143400_create_channels.rb
@@ -0,0 +1,9 @@
+class CreateChannels < ActiveRecord::Migration[6.1]
+  def change
+    create_table :channels do |t|
+      t.string :name
+      t.string :description
+      t.timestamps
+    end
+  end
+end
